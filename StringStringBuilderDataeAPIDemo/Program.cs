04e6c11df1ac0072c11aab1d9494874272777d25
deoresulakshana@gmail.com <36725147+sulakshanadeore@users.noreply.github.com>
2022-08-24 15:21:23 +0530

    Add project files.

---
+++ StringStringBuilderDataeAPIDemo/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringStringBuilderDataeAPIDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Enter Date");
+            //DateTime dt = new DateTime();
+            //dt = Convert.ToDateTime(Console.ReadLine());
+            //Console.WriteLine("Day=  " + dt.Day);
+            //Console.WriteLine("Month=  " + dt.Month);
+            //Console.WriteLine("Year=" + dt.Year);
+            //Console.WriteLine("Hour = " + dt.Hour);
+            //Console.WriteLine("Add 2 Days " +dt.AddDays(2));
+            //Console.WriteLine("Day of year "  + dt.DayOfYear);//236
+            //Console.WriteLine("Local time = " + dt.ToLocalTime());
+            DateTime dt1 = new DateTime();
+            dt1 = DateTime.Now;
+            Console.WriteLine(dt1);
+            Console.WriteLine("Long Date =" + dt1.ToLongDateString());
+            Console.WriteLine("Short Date=" + dt1.ToShortDateString());
+            Console.WriteLine("Long Time =" + dt1.ToLongTimeString());
+            Console.WriteLine("Short Time =" + dt1.ToShortTimeString());
+            Console.WriteLine("Add 2 Days " + dt1.AddDays(-2));
+
+            Console.WriteLine("--------------------------------");
+            DateTime dt2 = new DateTime(2022, 08, 23);
+           TimeSpan time_sp= dt1.Subtract(dt2);
+            Console.WriteLine(time_sp);
+            Console.WriteLine("----------------------------------");
+
+            DateTime utcTime=dt1.ToUniversalTime();
+                        Console.WriteLine(utcTime);
+            Console.WriteLine("----------------");
+            Console.WriteLine("Is Leap Year");
+            bool isLeapYear=DateTime.IsLeapYear(2008);
+            Console.WriteLine(isLeapYear);
+            Console.WriteLine("----------------------");
+            Console.WriteLine(DateTime.Today);
+            Console.WriteLine("------------------------");
+            
+            Console.ReadLine();
+                
+
+
+
+        }
+    }
+}
---
+++ StringStringBuilderDataeAPIDemo/StringDemo.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringStringBuilderDataeAPIDemo
+{
+    class StringDemo
+    {
+        static void Main(string[] args)
+        {
+            string s = "Welcome";
+            int len=s.Length;
+            Console.WriteLine(len);
+            s = "  Welcome    ";
+            len = s.Length;
+            Console.WriteLine("Before Trim=" + len);
+                         s=s.Trim();
+            len=s.Length;
+            Console.WriteLine("After Trim=" + len);
+            Console.WriteLine("Substring=");
+            string s1=s.Substring(0, 3);
+            Console.WriteLine(s1);
+            string s2 = s.Substring(4);
+            Console.WriteLine(s2);
+            int indexOfC=s.IndexOf('c');
+            Console.WriteLine("Index of c");
+            Console.WriteLine(indexOfC);
+            string newStr = "Welcome, to Mphasis, C#";
+            string[] sep = { "a", "," };
+            //char[] sep = { ' ' };
+            int cnt = 2;
+            string[] result=newStr.Split(sep, cnt, StringSplitOptions.RemoveEmptyEntries);
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("---------String Builder Examples------------");
+            //string---immutable
+            //It means string cannot be changed once crated 
+            string str = "Hello";
+            str = "Hello from me";//creates new object on the heap, instead of changing the previous value of the string at the same memory location.
+
+            //StringBuilder---mutable
+            //StringBuilder--in the System.Text namespace
+            StringBuilder b = new StringBuilder("Hello World ");
+            b.Append("Mphasis");
+            //for (int i = 0; i < b.Length; i++)
+            //{
+            //    Console.Write(b[i]);
+            //}
+            var sb=b.ToString();
+            Console.WriteLine(sb);
+            Console.WriteLine("---------------");
+            StringBuilder sb1 = new StringBuilder();
+            sb1.Append("Hello ");
+            sb1.AppendLine("World");
+            sb1.AppendLine("Hello from C#");
+            Console.WriteLine(sb1);
+            Console.WriteLine("----------------");
+
+            StringBuilder f1 = new StringBuilder("Your total bill amt= ");
+            f1.AppendFormat("{0:C} ", 30);
+            f1.AppendFormat("on todays date {0:d}",DateTime.Now.Date);
+            Console.WriteLine(f1);
+
+            Console.WriteLine("---------------B2-------------");
+            StringBuilder b2 = new StringBuilder("Hello World");
+            b2.Insert(5, ".NET");
+            Console.WriteLine(b2);
+            b2.Remove(4, 2);
+
+            Console.WriteLine("After remvoing= " + b2);
+            b2.Replace("NET", "C#");
+            Console.WriteLine("After replacing= ");
+            Console.WriteLine(b2);
+
+
+
+
+
+
+
+
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
